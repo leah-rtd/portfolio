636e652106f86a260877599cc279dd8cc05fef25
Leah Rothschild <leahisrothschild@gmail.com>
2026-01-10 20:15:49 +0100

    correctly did the resume thing

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,13 @@ st.set_page_config(page_title='Leah Rothschild' ,layout="wide",page_icon='👧�
                    initial_sidebar_state = "expanded")
 
 home_page = st.Page(Path("pages/Home.py"), title = "Home", icon = "🏠")
-resume_page = st.Page(Path("pages/Resume.py"), title = "Resume", icon = "💼")
+# resume_page = st.Page(Path("pages/Resume.py"), title = "Resume", icon = "💼")
 hobby_page = st.Page(Path("pages/Hobbies.py"), title = "Hobbies", icon = "⛷️")
 teaching_content_page = st.Page(Path("pages/Teaching_Materials.py"), title = "Teaching Materials", icon = "👩‍🏫")
 
-pg = st.navigation([home_page, resume_page, teaching_content_page, hobby_page])
+pg = st.navigation([home_page,
+                    # resume_page,
+                    teaching_content_page,
+                    hobby_page])
 
 pg.run()
